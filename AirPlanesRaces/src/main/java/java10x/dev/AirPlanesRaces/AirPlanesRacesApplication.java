90d19a6e3bf7199a5c43cb729868dc11d51d7f55
Yago Castello <yagodealmeidacs@gmail.com>
2026-06-16 13:12:52 -0300

    primeiro_commit
    primeiro commit

---
+++ AirPlanesRaces/src/main/java/java10x/dev/AirPlanesRaces/AirPlanesRacesApplication.java
@@ -0,0 +1,13 @@
+package java10x.dev.AirPlanesRaces;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirPlanesRacesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AirPlanesRacesApplication.class, args);
+	}
+
+}
---
+++ AirPlanesRaces/src/test/java/java10x/dev/AirPlanesRaces/AirPlanesRacesApplicationTests.java
@@ -0,0 +1,13 @@
+package java10x.dev.AirPlanesRaces;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AirPlanesRacesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
